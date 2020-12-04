ad33ab5521d797a6fd0542da81b6918a8e5418bb
heltoncoelho <motakoelho@gmail.com>
2020-12-03 23:26:33 -0300

    Update Printf.c

--- Básico/Printf.c
+++ Básico/Printf.c
@@ -5,5 +5,6 @@ int main(){
   printf("Hello World");
 
   //esse comentario foi adicionado dentro do github desktop para teste. Veremos.
+  //esse comentario foi adicionado no SUBLIMETEXT
   return 0;
 }
