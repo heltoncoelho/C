8942d89af105dd0c63c0b459a97583bf20cb241e
heltoncoelho <motakoelho@gmail.com>
2020-12-07 19:19:12 -0300

    New files

---
+++ Básico/CursoUdemy/condchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+	char letra = 'x';
+
+	if(letra =='x')
+		printf("\nLetra %c",letra);
+
+	printf("\nCodigo da letra: %d",letra);
+
+
+
+
+	printf("\n");
+	return 0;
+}
