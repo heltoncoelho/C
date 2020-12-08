17c865a756bf5db9722a481f00c2bd89de4f91c0
heltoncoelho <motakoelho@gmail.com>
2020-12-08 14:42:33 -0300

    New files

---
+++ Básico/CursoUdemy/condicionais3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+
+	int a = 10;
+
+	//and
+	if(a > 0 && a > 15)
+		printf("\nA entre 5 e 15");
+
+	//or
+	if(a > 1 || a==0)
+		printf("\nA maior que 1 ou igual a 0");
+
+
+
+
+
+
+	return 0;
+}
