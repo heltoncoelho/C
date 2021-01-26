467ad3b161373e774b6578c3e612bdbbac0d190b
heltoncoelho <motakoelho@gmail.com>
2021-01-26 14:32:59 -0300

    variáveis e expressões

---
+++ Listas de Exercícios/variáveis e expressões/45.c
@@ -0,0 +1,21 @@
+//Faça um programa para converter uma letra maiúscula em minúscula. Use a tabela ASCII
+
+#include <stdio.h>
+
+int main(){
+
+	char letra;
+	int minuscula;
+
+	printf("Digite uma letra maiuscula: ");
+	scanf("%c",&letra);
+
+	minuscula = letra + 32;
+
+	printf("Letra digitado em minusculo: %c\n",minuscula);
+
+
+
+
+	return 0;
+}
