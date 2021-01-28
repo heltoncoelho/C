494bb424ff9fb566d6a6a69ec7aba687100be8ce
heltoncoelho <motakoelho@gmail.com>
2021-01-28 10:45:01 -0300

    variáveis e expressões

---
+++ Listas de Exercícios/variáveis e expressões/47.c
@@ -0,0 +1,19 @@
+//leia um inteiro entre 1000 e 9999 e imprima um dígito por linha.
+
+#include <stdio.h>
+
+int main(){
+
+
+	int a,b,c,d;
+
+	printf("Digite um numero inteiro entre 1000 e 9999 separando os algarismos por espacos: ");
+	scanf("%d%d%d%d",&a,&b,&c,&d);
+
+	printf("%d\n",a);
+	printf("%d\n",b);
+	printf("%d\n",c);
+	printf("%d\n",d);
+
+	return 0;
+}
