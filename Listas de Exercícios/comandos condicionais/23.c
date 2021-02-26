7c2c25fb7f3b88a62b566edb5d8100a40ad90bac
heltoncoelho <motakoelho@gmail.com>
2021-02-26 18:38:06 -0300

    comandos condicionais

---
+++ Listas de Exercícios/comandos condicionais/23.c
@@ -0,0 +1,22 @@
+/*Determine se um ano lido é bissexto. Passos para determinar se um ano é bissexto:
+1. Divisivel por 400 sem resto, é bissexto.
+2. Divisivel por 4 sem resto mas nao por 100, é bissexto.*/
+
+
+#include <stdio.h>
+
+int main(){
+
+	int ano;
+
+	printf("Digite o ano para saber se eh bissexto: ");
+	scanf("%d",&ano);
+
+	if(ano%400 == 0)
+		printf("Ano bissexto, divisivel por 400");
+	else if (ano%4 == 0 && ano%100 !=0)
+		printf("Ano bissexto, divisivel por 4");
+	else
+		printf("O ano NAO eh bissexto.");
+	return 0;
+}
