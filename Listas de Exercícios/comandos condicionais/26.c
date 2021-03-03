0642bef4b134dca4608447af21a821b7ab3ccbd7
heltoncoelho <motakoelho@gmail.com>
2021-03-03 19:56:36 -0300

    comandos condicionais

--- Listas de Exercícios/comandos condicionais/26.c
+++ Listas de Exercícios/comandos condicionais/26.c
@@ -1,1 +1,26 @@
-/**/
+/*Leia um distancia em Km e a quantidade de gasolina consumida em um percurso. Calcule o consumo km/l*/
+
+#include <stdio.h>
+
+int main(){
+
+	int litros, distancia, consumo;
+
+	printf("Distancia percorrida: ");
+	scanf("%d",&distancia);
+
+	printf("Combustivel gasto (em litros): ");
+	scanf("%d",&litros);
+
+	consumo = distancia / litros;
+
+	if(consumo < 8)
+		printf("Consumo: %d km/l. Venda o carro!",consumo);
+	else if (consumo >= 8 && consumo <= 12)
+		printf("Consumo: %d km/l. Economico!",consumo);
+	else if (consumo > 12)
+		printf("Consumo: %d km/l. Super economico",consumo);
+
+
+	return 0;
+}
