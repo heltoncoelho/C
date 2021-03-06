715275f863f07364ce2d16645458144cfbc66fb4
heltoncoelho <motakoelho@gmail.com>
2021-03-06 17:05:51 -0300

    comandos condicionais

--- Listas de Exercícios/comandos condicionais/28.c
+++ Listas de Exercícios/comandos condicionais/28.c
@@ -1,1 +1,27 @@
-/*Fazer amanha*/
+/*Leia três números positivos e calcula uma das médias de acordo com um valor digitado pelo usuário:
+1 - geométrica
+2 - ponderada
+3 - harmonica
+4 - aritmética
+
+PS: fórmulas no .pdf*/
+
+#include <stdio.h>
+
+int main(){
+
+	int x,y,z;
+
+	printf("Digite um numero x positivo: ");
+		scanf("%d",&x);
+	printf("Digite um numero y positivo: ");
+		scanf("%d",&y);
+	printf("Digite um numero z positivo: ");
+		scanf("%d",&z);
+
+
+			//CONTINUAR NO DOMINGO
+
+
+	return 0;
+}
