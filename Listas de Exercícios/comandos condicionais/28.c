fb9a532984f121e8968e7bc845475a2a72c341ab
heltoncoelho <motakoelho@gmail.com>
2021-03-08 19:46:18 -0300

    comandos condicionais

--- Listas de Exercícios/comandos condicionais/28.c
+++ Listas de Exercícios/comandos condicionais/28.c
@@ -7,29 +7,30 @@
 PS: fórmulas no .pdf*/
 
 #include <stdio.h>
+#include <math.h>
 
 int main(){
 
-	int x,y,z, opt;
+	float x,y,z;
+	int opt;
 
 	printf("Digite um numero x positivo: ");
-		scanf("%d",&x);
+		scanf("%f",&x);
 	printf("Digite um numero y positivo: ");
-		scanf("%d",&y);
+		scanf("%f",&y);
 	printf("Digite um numero z positivo: ");
-		scanf("%d",&z);
+		scanf("%f",&z);
 
 	printf("\nDigite: 1 para média geometrica, 2 para ponderada, 3 para harmonica ou 4 para aritmetica: ");
 	scanf("%d",&opt);
 
 	switch(opt){
-		case 1: break;
-		case 2: break;
-		case 3: break;
-		case 4: break;
+		case 1: printf("Geometrica: %.2f", pow((x*y*z),(1/3)));break;
+		case 2: printf("Ponderada: %.2f",((x+2*y+3*z)/6));break;
+		case 3: printf("Harmonica: %.2f",(1/((1/x)+(1/y)+(1/z))));break;
+		case 4: printf("Aritmetica: %.2f",(x+y+z)/3);break;
 		default: printf("Erro.\n");
 	};
-
 
 
 	return 0;
