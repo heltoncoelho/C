b00a4f25423d2565f40a582da0e9842396b7c734
heltoncoelho <motakoelho@gmail.com>
2021-03-26 17:55:17 -0300

    comandos condicionais

--- Listas de Exercícios/comandos condicionais/37.c
+++ Listas de Exercícios/comandos condicionais/37.c
@@ -1,14 +1,34 @@
-/**/
+/*Faça um programa que calcule a tarifa a pagar de um estacionamento dado o tempo de uso. Ver regras no pdf*/
 
 #include <stdio.h>
 
 int main(){
 
-HELLO WORLD
 
-hoje estou muito cansado para terminar esse programa mas nao quero perder o habito de fazer o commit todo os dias
-então vai ficar para amanha.
+	//variaveis
+	int horaEntrada, minutoEntrada;
+	int horaSaida, minutoSaida;
 
+
+	//coletar os dados
+	printf("Hora de entrada:");
+	scanf("%d",&horaEntrada)
+
+	printf("Minuto de entrada:");
+	scanf("%d",&minutoEntrada);
+
+	printf("Hora de saida:");
+	scanf("%d",&horaSaida)
+
+	printf("Minuto de saida:");
+	scanf("%d",&minutoSaida);
+
+	//converter a diferença de tempo entre o momento de saída e o momento de chegada em minutos para ter o tempo de permanencia.
+
+	//converter o tempo de permanencia em minutos para horas
+
+	//calcular pela tabela no pdf o preço a pagar
+	
 
 
 
