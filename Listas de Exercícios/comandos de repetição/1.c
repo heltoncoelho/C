6a7724ced668be83a137883e8c7ccca6fd69a9b4
heltoncoelho <motakoelho@gmail.com>
2021-04-07 14:59:44 -0300

    comandos de repetição

--- Listas de Exercícios/comandos de repetição/1.c
+++ Listas de Exercícios/comandos de repetição/1.c
@@ -4,8 +4,20 @@
 
 int main(){
 
+	int indice;	
+	int contador = 0;
+
+	for(indice=3;indice<100;indice++){			
+
+			if(indice%3 == 0){	//busca pelos multiplos de 3 (cuja divisão por 3 não tem resto.)
+				printf("Multiplo: %d\n",indice);
+				contador++;				
+			};	
 
 
+			if(contador == 5)	//quando conseguirmos os 5 primeiros multiplos, saimos do laço FOR
+					break;		
+	};
 
 
 	return 0;
