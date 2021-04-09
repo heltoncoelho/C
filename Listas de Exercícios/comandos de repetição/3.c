30646d902e7345fb988f2964c051080dc2659ad6
heltoncoelho <motakoelho@gmail.com>
2021-04-09 09:51:35 -0300

    comandos de repetição

---
+++ Listas de Exercícios/comandos de repetição/3.c
@@ -0,0 +1,18 @@
+/*Faça um algoritmo usando while que mostre uma contagem regressiva de 10 a 0. Mostrar a mensagem "fim" ao término.*/
+
+#include <stdio.h>
+
+int main(){
+
+	int indice = 10;
+
+	while(indice != 0){
+		printf("%d ",indice);
+		indice--;
+	};
+
+	printf("FIM!");
+
+
+	return 0;
+}
