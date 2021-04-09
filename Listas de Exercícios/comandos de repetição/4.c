a35b29af4fcf3c9e757835c0ea20b79b04af05ca
heltoncoelho <motakoelho@gmail.com>
2021-04-09 20:16:04 -0300

    comandos de repetição

---
+++ Listas de Exercícios/comandos de repetição/4.c
@@ -0,0 +1,15 @@
+/*Declare um inteiro inicializando-o com 0. Incremente-o de 1k em 1k até somar o total de 100k. Imprima na tela.*/
+
+#include <stdio.h>
+
+int main(){
+
+	int hPLovecraft = 0;
+
+	while(hPLovecraft != 100000){		
+		hPLovecraft += 1000;
+		printf("%d\n",hPLovecraft);
+	};
+
+	return 0;
+}
