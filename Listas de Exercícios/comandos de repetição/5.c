dfd335b5957895b41953db6ec52b36d10f27542b
heltoncoelho <motakoelho@gmail.com>
2021-04-10 13:03:21 -0300

    comandos de repetição

---
+++ Listas de Exercícios/comandos de repetição/5.c
@@ -0,0 +1,21 @@
+/*Peça ao usuário para digitar 10 valores em sequência e some-os. Imprima na tela o resultado*/
+
+#include <stdio.h>
+
+int main(){
+
+	int indice, valor;
+	int soma = 0;
+
+	for(indice=1;indice<=10;indice++){
+		printf("Valor %d: ",indice);
+		scanf("%d",&valor);
+		soma += valor;
+	};
+
+	printf("\nSoma dos valores digitados: %d",soma);
+
+
+
+	return 0;
+}
