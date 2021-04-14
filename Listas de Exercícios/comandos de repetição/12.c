e9d8a50ae2713c0598dca291fb3a89660cea9292
heltoncoelho <motakoelho@gmail.com>
2021-04-14 20:34:56 -0300

    comandos de repetição

---
+++ Listas de Exercícios/comandos de repetição/12.c
@@ -0,0 +1,22 @@
+/*Leia um inteiro positivo N e imprima todos os números até N em ordem decrescente.*/
+
+#include <stdio.h>
+
+int main(){
+
+	int inteiroN = 0;
+	int indice = 0;
+
+	while(inteiroN<=0){
+		printf("Digite um numero inteiro: ");
+		scanf("%d",&inteiroN);		
+	};
+
+	indice = inteiroN;
+	while(indice != 0){
+		printf("%d ",indice);
+		indice--;
+	};
+
+	return 0;
+}
