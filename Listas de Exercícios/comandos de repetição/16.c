fdb8c9300efbf1ef4eb37a22a411632eda1dd127
heltoncoelho <motakoelho@gmail.com>
2021-04-16 10:09:42 -0300

    comandos de repetição

---
+++ Listas de Exercícios/comandos de repetição/16.c
@@ -0,0 +1,24 @@
+/*Leia um inteiro positivo impar N e imprima todos os ímpares de 1 até N em ordem decrescente*/
+
+#include <stdio.h>
+
+int main(){
+
+	int inteiro;
+	int indice;
+
+
+	do{
+		printf("Digite um inteiro positivo impar: ");
+		scanf("%d",&inteiro);
+	}while(inteiro%2 == 0);
+
+
+	for(indice=inteiro;indice!=0;indice--){		
+		if(indice%2 != 0)
+			printf("%d ",indice);
+	};
+
+
+	return 0;
+}
