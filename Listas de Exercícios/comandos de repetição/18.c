9dd9e768ecd24049a7ebdeedf94b659fcbb4ded2
heltoncoelho <motakoelho@gmail.com>
2021-04-18 09:59:51 -0300

    comandos de repetição

--- Listas de Exercícios/comandos de repetição/18.c
+++ Listas de Exercícios/comandos de repetição/18.c
@@ -1,12 +1,11 @@
-/*Leia uma certa quantidade de numeros e imprima o maior deles e quantas vezes o maior numero foi lido. A quantidade de numeros
-a serem lidos deve ser fornecida pelo usuario*/
+/*Leia uma certa quantidade de numeros e imprima o maior deles. A quantidade de numerosa serem lidos deve ser fornecida 
+pelo usuario*/
 
 
 #include <stdio.h>
 
 int main(){
 
-	int contador = 0;
 	int num;
 	int qtdNumeros;
 	int maior, i;
@@ -18,22 +17,19 @@ int main(){
 		printf("Numero #1: ");
 		scanf("%d",&maior);
 	};
+	
 
 	for(i=2;i<=qtdNumeros;i++){	//coleta os numeros até o limite n dado pelo usuario
 		printf("Numero #%d: ",i);
 		scanf("%d",&num);
 
-		if(num>=maior){	//compara o digitado, se for maior armazena
-			maior = num;
+		if(num>maior){	//compara o digitado, se for maior armazena
+			maior = num;	
 		};
-
-
 	};
 	
-	//NOTA: FALTA ADICIONAR UM CONTADOR PARA INDICAR QUANTAS VEZES O MAIOR NUMERO FOI DIGITADO. O RESTO ESTA PRONTO.
 
-	printf("Maior numero: %d",maior);
-
+	printf("Maior: %d",maior);
 
 	return 0;
 }
