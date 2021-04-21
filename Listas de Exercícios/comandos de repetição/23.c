04ac833ee67e4eed93f8828e50527f70326befac
heltoncoelho <motakoelho@gmail.com>
2021-04-21 15:07:34 -0300

    comandos de repetição

---
+++ Listas de Exercícios/comandos de repetição/23.c
@@ -0,0 +1,20 @@
+/*Leia um numero positivo e imprima seus divisores.*/
+
+#include <stdio.h>
+
+int main(){
+
+	int numeroPositivo;
+	int i;
+
+	printf("Digite um numero positivo: ");
+	scanf("%d",&numeroPositivo);
+
+	for(i=1;i<=numeroPositivo;i++){
+
+		if(numeroPositivo%i == 0)
+			printf("%d ",i);
+	};
+
+	return 0;
+}
