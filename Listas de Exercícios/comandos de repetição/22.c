e69b7f13f8d0a5bddc2133654af416f93845cb00
heltoncoelho <motakoelho@gmail.com>
2021-04-22 10:28:17 -0300

    comandos de repetição

--- Listas de Exercícios/comandos de repetição/22.c
+++ Listas de Exercícios/comandos de repetição/22.c
@@ -1,20 +1,23 @@
 /*O aluno deve introduzir uma sequência arbitraria de notas no intervalo de 10 a 20. O numero de notas nao é pré definido e o
-programa deve encerrar com a digitação de uma nota inválida. Imprimir a media aritmética*/
+programa deve encerrar com a digitação de uma nota inválida. Imprimir a media aritmética.*/
 
 #include <stdio.h>
+#include <stdbool.h>
 
 int main(){
 
+
+	bool check = true;
 	int somaNotas = 0;
 	int contaNotas = 0;
 	int mediaAritmetica = 0;
 	int nota = 11;	
 
-	while((nota>10) && (nota<20)){
+	while(check){
 		printf("Digite uma nota: ");
 		scanf("%d",&nota);
 
-		if(nota>10 && nota<20){
+		if(nota>=10 && nota<=20){
 		contaNotas++;
 		somaNotas += nota;
 		}else
@@ -25,7 +28,6 @@ int main(){
 	printf("\nMedia: %d",mediaAritmetica);
 
 
-	//NOTA: TRABALHAR MAIS NESSE PROGRAMA. VARIOS ERROS.
 
 
 	return 0;
