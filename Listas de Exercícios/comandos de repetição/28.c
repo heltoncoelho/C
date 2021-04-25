836acb4b676517f97780e2e536e656f65712e80d
heltoncoelho <motakoelho@gmail.com>
2021-04-25 13:04:00 -0300

    comandos de repetição

---
+++ Listas de Exercícios/comandos de repetição/28.c
@@ -0,0 +1,12 @@
+/*Leia uma valor inteiro e positivo N, calculo e mostre o valor E conforme a fórmula: E = 1 + 1/1! + 1/2! + ... 1/N!*/
+
+#include <stdio.h>
+
+int main(){
+
+	
+
+
+
+	return 0;
+}
