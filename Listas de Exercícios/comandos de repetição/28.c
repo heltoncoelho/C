50175570b1a1b1b7cfd6210915d058c5c0f5c27e
heltoncoelho <motakoelho@gmail.com>
2021-04-26 16:41:06 -0300

    comandos de repetição

--- Listas de Exercícios/comandos de repetição/28.c
+++ Listas de Exercícios/comandos de repetição/28.c
@@ -1,11 +1,38 @@
-/*Leia uma valor inteiro e positivo N, calculo e mostre o valor E conforme a fórmula: E = 1 + 1/1! + 1/2! + ... 1/N!*/
+/*Faça um programa que leia um valor N inteiro e positivo, calcule o mostre o valor E,
+conforme a fórmula a seguir E = 1 + 1/1! + 1/2! + 1/3! + ... + 1/N !*/
 
-#include <stdio.h>
+#include<stdio.h>
+
+//função para fatorar o denominador e retornar o valor fatorado.
+float fatora(float n){
+	float fat=1;
+	if(n>=0){		
+		while(n>0){
+			fat*=n;
+			n--;
+		}		
+	}else
+		return -1;
+
+	return fat;
+}
 
 int main(){
 
+	float n = 0;
+	int i = 0;
+	float soma = 0;
+	int max = 0;
 	
+	printf("Digite um inteiro N positivo: ");
+	scanf("%d",&max);
 
+	for(i=1;i<max;i++){
+		n = fatora(i);
+		soma+= 1/n;
+	}
+
+	printf("\nE = %f",soma);
 
 
 	return 0;
