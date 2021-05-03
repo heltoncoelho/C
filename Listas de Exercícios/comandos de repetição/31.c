a38db50d2654340acfc4ad6aa270017e2d5272d5
heltoncoelho <motakoelho@gmail.com>
2021-05-02 22:13:43 -0300

    comandos de repetição

--- Listas de Exercícios/comandos de repetição/31.c
+++ Listas de Exercícios/comandos de repetição/31.c
@@ -1,4 +1,4 @@
-/*Cansado. Amanha eu trabalho nisso. Farei um commit para não perder o hábito*/
+/*Faça um programa que calcule e escreva o valor de S = 1/1 + 3/2 + 5/3 + 7/4 ... 99/50*/
 
 #include <stdio.h>
 
