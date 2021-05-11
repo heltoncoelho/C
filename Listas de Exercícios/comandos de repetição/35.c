7cef72539deac4524bf034e9ae59c223093a964a
heltoncoelho <motakoelho@gmail.com>
2021-05-11 15:13:02 -0300

    comandos de repetição

--- Listas de Exercícios/comandos de repetição/35.c
+++ Listas de Exercícios/comandos de repetição/35.c
@@ -1,11 +1,34 @@
-/**/
+/*Faça um programa que some os numeros impares dentro de um intervalo definido pelo usuario. Caso o usuario digite um
+intervalo errado (final maior que o inicio) exibir mensagem de erro e sair do programa.*/
 
 #include <stdio.h>
 
 int main(){
 
+	int i, min, max, soma = 0;
 
+	printf("Valor inicial do intervalo: ");
+	scanf("%d",&min);
+
+	printf("Valor final do intervalo: ");
+	scanf("%d",&max);
+
+	if(max<=min){
+		printf("Erro.");
+		return 0;
+	}
+
+	for(i=min;i<=max;i++){
+
+		if(i%2 != 0){
+			printf("%d ",i);
+			soma+=i;
+		}
+	}
+
+	printf("\nSoma dos valores impares dentro do intervalo: %d",soma);
 
 
 	return 0;
+	
 }
